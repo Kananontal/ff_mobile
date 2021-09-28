8e95cc93502e01cea22c27127122eae778af6e2c
= <s6102041620038@email.kmutnb.ac.th>
2021-09-28 18:14:37 +0700

    Api

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -1,4 +1,8 @@
+import 'package:ff_mobile/model/activity.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+var data;
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -9,9 +13,52 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   @override
+  void initState() {
+    super.initState();
+    callActivity();
+  }
+
+  Future<void> callActivity() async {
+    print("Helloworld");
+
+    var url = Uri.parse("https://www.boredapi.com/api/activity");
+
+    var response = await http.get(url);
+    print(response.statusCode);
+    print(response.body);
+
+    data = cedFromJson(response.body); // Json to object
+    print(data.activity);
+    print(data.type);
+    print(data.participants);
+    print(data.price);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Dashboard'),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(Icons.api_sharp),
+              SizedBox(
+                width: 10,
+              ),
+              Text('Dashboard'),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            Text(data?.activity ?? "กำลังโหลด"),
+            Text(data?.type ?? ""),
+            Text('${data?.participants ?? ""}'),
+            Text('${data?.price ?? ""}'),
+          ],
+        ),
+      ),
     );
   }
 }
